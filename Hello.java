641a7443a13e56c0d5cd16c59ed52995fea29993
Kapil Dev <kapil.dev87@gmail.com>
2021-04-10 11:24:32 -0700

    Update Hello.java
    This is second commit to see the change log

--- Hello.java
+++ Hello.java
@@ -1,5 +1,6 @@
 public class Hello {
     public static void main(String[] args){
         System.out.println("Hello World! Welcome to Jenkins run"); 
+        System.out.println("This is second commit to see the change log!!");
     }
 }
